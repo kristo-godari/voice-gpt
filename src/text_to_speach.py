02b83ab9fe061e661c2037a590b1f688e46db2ab
kristo.godari@gmail.com <kristo.godari@gmail.com>
2023-07-09 18:08:22 +0200

    Added more models.

--- src/text_to_speach.py
+++ src/text_to_speach.py
@@ -5,8 +5,8 @@ class TextToSpeechConverter:
 
     def __init__(self, config):
         self.config = config
-        self.model_name = TTS.list_models()[0]
+        self.model_name = "tts_models/multilingual/multi-dataset/your_tts" if self.config.get_text_to_speech_lang() == "en" else "tts_models/de/thorsten/tacotron2-DDC"
         self.tts = TTS(self.model_name)
 
     def text_to_speech(self, text):
-        self.tts.tts_to_file(text=text, speaker=self.tts.speakers[1], language=self.tts.languages[0], file_path=self.config.get_text_to_speech_output_file())
+        self.tts.tts_to_file(text=text, speaker=self.tts.speakers[0], language=self.tts.languages[0], file_path=self.config.get_text_to_speech_output_file())
